8b6a979e60f03804e14df0bf71366c9a66c5f582
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-29 15:01:48 -0600

    adds code and learning summary to complete intro to JavaScript exercise 4

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,7 @@
+//Learned how to initiailize a variable essentially creating a new variable with a designated initial values
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
