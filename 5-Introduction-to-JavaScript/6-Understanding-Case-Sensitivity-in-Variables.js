537e178267d38069f892ea8f5f08ea3f699068c8
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-29 18:59:40 -0600

    adds code and learning summary to complete intro to JavaScript exercise 6

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+//Learned the importance of case sensitivity and the recomendation of using camelCase*/
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
