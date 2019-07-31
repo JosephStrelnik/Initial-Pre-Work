7860b8e418cba55d4bc64bc48d1185a35eb859cf
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 10:54:01 -0600

    adds code and learning summary to complete intro to JavaScript exeercise 20

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,11 @@
+//Learned how to use the /= operator to divide a variable by another number
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a = a /= 12;
+b = b /= 4;
+c = c /= 11;
