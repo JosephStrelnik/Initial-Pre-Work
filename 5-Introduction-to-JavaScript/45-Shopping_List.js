3c47add9b7c7820bc58d366e59538e2ec7c3c067
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 15:20:49 -0600

    adds code and learning summary to complete intro to JavaScript exercise 45

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,3 @@
+//Learned how to create a multi dimensional array and sub arrays in the form of a basic shopping list
+
+var myList = [["Chocolate Bar", 15],["Candy", 8],["Water", 20],["Bread", 15],["Milk", 4],["Limes", 3]];
