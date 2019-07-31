db5df47272628b3c9c06926cf95746cf93e596b1
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 12:15:52 -0600

    adds code and learning summary to complete intro to JavaScript exercise 30

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+//Learned how to use bracket notation and how it relates to zero based indexing
+
+/ Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
