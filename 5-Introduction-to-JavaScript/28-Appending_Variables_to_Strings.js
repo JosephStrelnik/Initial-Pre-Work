2e99c2b0489b49dfe89310d8694ec70a5493894b
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 12:10:27 -0600

    adds code and learning summary to complete intro to JavaScript exercise 28

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+//Learned how to use the += operator to append variables to a strings
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "great";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
