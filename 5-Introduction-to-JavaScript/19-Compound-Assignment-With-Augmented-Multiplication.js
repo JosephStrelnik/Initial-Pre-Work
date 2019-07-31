aa918fd0b14fa51d0daf279a267a70128ed4fc5a
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 10:51:46 -0600

    adds code and learning summary to complete intro to JavaScript exercise 19

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,11 @@
+//Learned how to use the *= operator to multiply a variable by a numbers
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a = a *= 5;
+b = b *= 3;
+c = c *= 10;
