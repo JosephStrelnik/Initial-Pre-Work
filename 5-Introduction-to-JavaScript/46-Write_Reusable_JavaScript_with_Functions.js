32b0756082594fddbcc834eee3e5da0f9f27aa3d
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 15:27:11 -0600

    adds code and learning summary to complete intro to JavaScript exercise 46

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+//Learned how to create and utilise a reusible function
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
