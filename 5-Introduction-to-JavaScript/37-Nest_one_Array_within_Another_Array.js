e1e24db5e88ef67659cb2632bf216e82f9adfc4c
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 14:20:07 -0600

    adds code and learning summary to complete intro to JavaScript exercise 37

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+//Learned how to create a Multi dimensional Array by nesting one array within another
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Steelers",7],["Pirates",86]];
