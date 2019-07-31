b15beb101be604f0881d4222e7bae06576e9ae16
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 12:28:08 -0600

    adds code and learning summary to complete intro to JavaScript exercise 31

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+//Learned that string values are immutable or unchanging but can be changed by adding a new string
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
