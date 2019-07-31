af4fda9e42e13608d96263c2b746694eab5e6f79
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 11:55:04 -0600

    adds code and learning summary to complete intro to JavaScript exercise 26

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+//Learned how to use the += operator to concatenate a string to the end of an existing strings variable
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
