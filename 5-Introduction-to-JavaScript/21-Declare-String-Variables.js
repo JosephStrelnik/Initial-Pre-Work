583cd91c65be6162051bc95ef202620b76f8aee1
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 11:04:32 -0600

    adds code and learning summary to complete intro to JavaScript exercise 21

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+//Learned the definition of string literal and practiced creating a new String
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Joseph";
+var myLastName = "Strelnik";
