4c01fa3f35bf835c12cf4b62c4b6e0820c4ba488
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 12:11:12 -0600

    adds code and learning summary to complete intro to JavaScript exercise 29

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,15 @@
+//Learned how to use .length find the length of a string
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
