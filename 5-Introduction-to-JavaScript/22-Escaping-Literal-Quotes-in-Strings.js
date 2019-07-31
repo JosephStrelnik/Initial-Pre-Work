3d3b32ebce45bd1f3722482097a95dec34435592
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 11:08:14 -0600

    adds code and learning summary to complete intro to JavaScript exercise 22

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+//Learned \\ to escape a quote within a string so it doesnt recognize the internal quote as an end of a string
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
