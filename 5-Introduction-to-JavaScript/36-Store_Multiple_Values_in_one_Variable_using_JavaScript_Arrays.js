bc99e419405633b7913db342b86bf0f2b97c306d
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 13:55:16 -0600

    adds code and learning summary to complete intro to JavaScript exercise 36

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+//Learned how to store multiple pieces of data in one place using array
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Joe", 77];
