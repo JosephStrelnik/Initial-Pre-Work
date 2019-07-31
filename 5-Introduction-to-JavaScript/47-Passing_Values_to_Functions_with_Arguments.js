34706e1010849176a544ec00d2d03fb1baef61d8
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 15:35:52 -0600

    adds code and learning summary to complete intro to JavaScript exercise 47

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,14 @@
+//Learned the role of parameters as placeholders for values and how arguments can be passed into a function and called
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(a,b) {
+  console.log(a + b)
+}
+functionWithArgs(10, 2);
