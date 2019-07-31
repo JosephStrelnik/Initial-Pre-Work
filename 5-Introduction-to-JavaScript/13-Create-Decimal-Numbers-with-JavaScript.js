0465c77c776f6c4b908c11ec2e1b31884827020c
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 10:06:26 -0600

    adds code and learning summary to complete intro to JavaScript exercise 13

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,7 @@
+//Learned  how to store floating numbers (decimal numbers) in variables and also reviewed the floating point arithmatic wiki page
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
