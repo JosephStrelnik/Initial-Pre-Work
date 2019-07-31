7236d92aa6f048366f7dc5f04f360eff8d7ef446
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 09:59:47 -0600

    adds code and learning summary to complete intro to JavaScript exercise 12

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+//Learned how to decrement or decrease a variable by one simply by using the-- operator. This eliminates the need for an equal assign
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
