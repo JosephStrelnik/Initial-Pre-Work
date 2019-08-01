2db205ad9d1190b77c10bd1b88bfcec2222b89a9
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 14:46:31 -0600

    adds code and learning summary to complete intro to JavaScript exercise 104

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,7 @@
+//Learned how use and understand the parseInt() function to parse as string and return an intiger
+
+function convertToInteger(str) {
+return parseInt(str)
+}
+
+convertToInteger("56");
