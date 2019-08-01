b1a2094f360753d56937997bfc69049c10121a3d
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 11:41:42 -0600

    adds code and learning summary to complete intro to javaScript 81

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+//Learned how to use dot notation and bracket notation to access properties of an object
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
