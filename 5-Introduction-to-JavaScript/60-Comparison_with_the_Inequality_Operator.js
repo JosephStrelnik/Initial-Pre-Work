120f35056622bf4e118d6068d85891a180fec36b
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 08:19:53 -0600

    adds code and learning summary to complete intro to JavaScript exercise 60

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+//Learned what the inequality operator (!=) is and how it relates in usage and definition versus the equality operator
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
