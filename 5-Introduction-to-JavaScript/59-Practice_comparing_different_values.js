6ea4c8a01c593c32f4ba559fdf987251740178b5
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 08:15:37 -0600

    adds code and learning summary to complete intro to JavaScript exercise 59

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+//Learned more in depth comparisons and practice about equality operator and strict equality operator in relation to data type as well as a brief intro to the typeof operator
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
