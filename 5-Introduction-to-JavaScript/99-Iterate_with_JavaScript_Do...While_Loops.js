ee784b0ad5a74fadf505e5196ea9c8dc2e39f02f
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 13:37:42 -0600

    adds code and learning summary to complete intro to JavaScript exercise 99

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,15 @@
+//Learned what and how to use a do...while loop
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+var myArray = [];
+var i = 10;
+
+do {
+myArray.push(i)
+i++
+} while (i < 5)
