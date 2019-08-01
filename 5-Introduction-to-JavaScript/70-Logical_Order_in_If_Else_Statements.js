397b9a7f7e5e18261f2262442846cd1189356e6b
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 09:56:56 -0600

    adds code and learning summary to complete intro to JavaScript exercise 70

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,14 @@
+//Learned the importance of order in regards to if, else if statements and understanding that functions are executed from top to bottom so it is important to understand which statement to put first
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
