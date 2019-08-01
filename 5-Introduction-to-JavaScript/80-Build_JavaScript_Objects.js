f2b8669a24449fabdb9e33bcc3aab8a6694b1a8d
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 11:37:28 -0600

    adds code and learning summary to complete intro to JavaScript exercise 80

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,20 @@
+//Learned what an object is and how it differs from arrays via the use of proerties to access object data
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Sampson",
+  "legs": 3,
+  "tails": 2,
+  "friends": ["everyone!"]
+
+
+};
