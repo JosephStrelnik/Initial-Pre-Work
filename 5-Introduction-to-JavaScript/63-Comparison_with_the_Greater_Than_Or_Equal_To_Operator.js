a7ccdbfbefd22dcb19eb08519385e1af6d7247ba
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 08:43:48 -0600

    adds code and learning summary to complete intro to JavaScript exercise 63

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,16 @@
+//Learned how to use the greter than or equal to operator (>=) and that it will convert data types while comparing
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
