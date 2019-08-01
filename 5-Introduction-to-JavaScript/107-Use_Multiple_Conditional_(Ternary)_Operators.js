e4ac4c0d7618ce7ab0bca348faed5b7581ae6310
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 15:07:58 -0600

    adds code and learning summary to complete intro to JavaScript exercise107

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,9 @@
+//Learned how to use multiple conditional operators to check multiple conditions.
+
+function checkSign(num) {
+ return(num==0)?"zero":
+ (num>0)?"positive":
+ (num<0)?"negative":
+
+
+checkSign(10);}
