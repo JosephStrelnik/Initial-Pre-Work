0a4c1496d36aad3bc30629ec84feaf73b4bad325
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 18:37:48 -0600

    adds code and learning summary to complete intro to JavaScript exercise 52

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,17 @@
+//Learned how create a function without a return statement that creates a return value that is undefined
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+var sum = 0;
+function addFive() {
+  sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
