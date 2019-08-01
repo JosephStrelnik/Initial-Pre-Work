e0ffcc4f3e627c6dce3ad8089b92f6554a6ad299
Joseph Strelnik <jstrelnik@hotmail.com>
2019-07-31 18:31:31 -0600

    adds code and learning summary to complete intro to JavaScript exercise 51

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,13 @@
+//Learned how to use a return statement to send a value back to a function
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+console.log(timesFive(10));
+console.log(minusSeven(10));
