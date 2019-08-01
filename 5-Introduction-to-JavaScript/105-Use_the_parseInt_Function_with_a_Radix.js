e54c1c33ca45f75c755b84fe6d043f9a9d63c091
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 14:47:26 -0600

    adds code  and learning summary to complete intro to JavaScript exercise 105

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,8 @@
+// learned what radix is and how it specifies the base number of a strings
+
+function convertToInteger(str) {
+var radix = 2;
+return parseInt(str, radix);
+}
+
+convertToInteger("10011");
