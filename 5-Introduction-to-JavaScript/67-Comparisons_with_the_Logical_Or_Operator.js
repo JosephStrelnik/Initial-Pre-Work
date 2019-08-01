40a1788c7dd01ff9c4ad16053e2aacc200ba3d89
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 09:13:14 -0600

    adds code and learning summary to complete intro to JavaScript exercise 67

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,15 @@
+//Learned how to use the logical or operator (||) to use either operand to complete a true/false statements
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val  > 20 || val < 10) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
