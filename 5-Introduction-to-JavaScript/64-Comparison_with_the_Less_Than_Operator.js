04162622f8b2222c4d6b65d47955e3cf68fe3365
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 08:47:56 -0600

    adds code and learning summary to complete intro to JavaScript exercise 64

---
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -0,0 +1,16 @@
+//Learned how to use the leas then operator(<) in the same manor as the greater than operator (>)and that it to will convert data types while comparing
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
