922f5ec208e1a05e9e7b4c8ad1f74d3d9c87cbe1
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 13:02:38 -0600

    adds code and learning summary to complete intro to JavaScript exercise 93

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+//Learned how to use a loop to run the same code multiple times. The exercise uses a while loop to run while a specified condition is true intil it is not
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while (i <= 4){
+    myArray.push(i);
+    i++;
+}
