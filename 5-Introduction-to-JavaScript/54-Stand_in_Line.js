2302881cc05a5cf90cc259dbae97314839cf2487
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 07:21:19 -0600

    adds code and learning summary to complete intro to JavaScript exercise 54

---
+++ 5-Introduction-to-JavaScript/54-Stand_in_Line.js
@@ -0,0 +1,15 @@
+//Learned how to manipulate a queue using an array and number as arguments to essentially remove the first element of an array and adding an element to the end of the array
+
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  return arr.shift();  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
