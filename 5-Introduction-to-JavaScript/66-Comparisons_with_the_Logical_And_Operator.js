3961d850ae88717f2e2561dfbfe80ab838c0be5d
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 09:04:40 -0600

    adds code and learning summary to complete intro to JavaScript exercise 66

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,16 @@
+//Learned how to test more than one thing at a time using the logical and (&&) operator by adding multiple operands
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+      return "Yes";
+    }
+
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
