0b0197459a1eadb18fa5f465c2225a4ed5b33ab5
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 08:25:10 -0600

    adds code and learning summary to complete intro to JavaScript exercise 61

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+//Learned about the strict inequality operator (!==) and how it can be used similarly to the strict equality operator with opposite logic
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
