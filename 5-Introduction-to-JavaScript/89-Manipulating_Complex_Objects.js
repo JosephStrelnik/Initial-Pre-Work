744b92723cad06ebfc65af9df2484a4643241002
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 12:34:42 -0600

    adds code and learning summary to complete intro to JavaScript exercise 89

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+//Learned how to use an object as a flexible data structure using combos of strings, arrays, functions etc.
+
+var myMusic = [
+ {
+   "artist": "Billy Joel",
+   "title": "Piano Man",
+   "release_year": 1973,
+   "formats": [
+     "CD",
+     "8T",
+     "LP"
+   ],
+   "gold": true
+ },
+ // Add record here
+ {
+   "artist": "My Band",
+   "title": "My Album",
+   "release_year": 1977,
+   "formats": [
+     "CD",
+     "8T",
+     "LP"
+   ],
+ }
+];
