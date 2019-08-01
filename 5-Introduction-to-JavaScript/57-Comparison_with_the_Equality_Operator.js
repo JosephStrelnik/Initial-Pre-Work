8e3122f3d03886518e73420cf7a859965b6d8da9
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 08:04:55 -0600

    adds code and learning summary to complete intro to JavaScript exercise 57

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+//Learned comparison operators return a boolean value and learned how to use the equality operator (==) which differs from assignment (=)
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
