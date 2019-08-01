3e7cb58c06298cef8ba2c7ba2702503a5a4b7923
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 07:52:05 -0600

    adds code and learning summary to complete intro to JavaScript exercise 55

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+//Learned what boolean values refer (True/False) to and how to use them correctly
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
