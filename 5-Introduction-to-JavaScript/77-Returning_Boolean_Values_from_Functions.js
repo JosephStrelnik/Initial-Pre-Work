c9317d42db4f4b228e15002feb75ed7bfd04f6fb
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 11:17:06 -0600

    adds code and learning summary to complete intro to JavaScript exercise 77

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,10 @@
+//Learned how to return boolean values using if/else statements or more effectively, using === to return true/False
+
+function isLess(a, b) {
+  // Fix this code
+  return a <= b;
+
+}
+
+// Change these values to test
+isLess(10, 15);
