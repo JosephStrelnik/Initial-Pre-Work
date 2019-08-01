5a14d86ae10bebe359f01110aa827cb3d46440eb
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 08:31:00 -0600

    adds code and learning summary to complete intro to JavaScript exercise 62

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,16 @@
+//Learned how to use the greater than operator (>) to provide a boolean response of true/false
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);
