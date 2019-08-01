3b145b78dfe5b66a1c86a4bacb660d7c67d08587
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 11:45:33 -0600

    adds code and learning summary to complete intro to JavaScript exercise 82

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+//Learned how to strictly use bracket notation to access object properties that do and do not have spaces
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
