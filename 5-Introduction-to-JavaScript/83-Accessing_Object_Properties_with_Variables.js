6e425309dd51145a57f73b2b575b67222b8be2aa
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 11:50:17 -0600

    adds code and learning summary to complete intro to JavaScript exercise 83

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+//Learned how to use brackets to access a property that is stored as the value of a variable or when a propertys name is collected dynamically during program execution
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
