3cf4e4c9da49ee39da2003d00d5eb5a12f326d5f
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 13:12:18 -0600

    adds code and learning summary to complete intro to JavaScript exercise 95

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,16 @@
+//Learned how to change initialization to odd numbers by changing the final-expression
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i += 2) {
+myArray.push(i);
+}
