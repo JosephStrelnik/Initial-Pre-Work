3e20c6d487b92646253442759452dc82f5193f2f
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 13:07:54 -0600

    adds code and learning summary to complete intro to JavaScript exercise 94

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+//Learned to use a for loop to run code multiple times and how to declare them with initialization, condition, final-expression
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var myArray = [];
+for(var i=1; i<=5; i++){
+myArray.push(i);
+}
