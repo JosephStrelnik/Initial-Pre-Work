3370025c5d202a88af37e6c1b14aef8652429674
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-01 12:43:02 -0600

    adds code and learning summary to complete intro to JavaScript exercise 90

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+//Learned how to access an objects sub properties by chaining together dot or bracket notation
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
