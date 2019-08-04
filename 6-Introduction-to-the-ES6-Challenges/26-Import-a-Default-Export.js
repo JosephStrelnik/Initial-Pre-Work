7ee46f64a8ae1ef356500b7558f1c664414ec761
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-04 11:39:09 -0600

    adds code and learning summary to complete intro to ES6 challenges exercise 26

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,5 @@
+//Learned how to import a default export using different syntax by not using curly braces to surround the imported value
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
