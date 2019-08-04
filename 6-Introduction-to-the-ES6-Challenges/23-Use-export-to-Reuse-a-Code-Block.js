55c6dc72a3f633461d78e3e5af2e9a1e2e733c12
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-04 11:22:44 -0600

    adds code and learning summary to complete intro to ES6 challenges exercise 23

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,5 @@
+//Learned how to use export in conjunction with import to benefit from the speed and memory perks by only using some code vs all to be usable by another file
+
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
