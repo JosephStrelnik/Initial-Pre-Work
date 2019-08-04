91aa51f52730ae2ca51487c247d919514f98e78d
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-04 11:27:25 -0600

    adds code and learning summary to complete intro to ES6 challenges exercise 24

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,4 @@
+//Learned how to use import * as to import the entire contents of a file to an object that receives import values, called by dot notation
+
+"use strict";
+import * as str from "capitalize_strings";
