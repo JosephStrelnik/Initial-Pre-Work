9220d051fb2daccbdc23b5bf1608a251b510a69c
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-04 11:34:19 -0600

    adds code and learning summary to complete intro to ES6 challenges exercise 25

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,4 @@
+//Learned what is and how to use export default typically if one value is being exported from a file and or a fallback value for a file or module
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
