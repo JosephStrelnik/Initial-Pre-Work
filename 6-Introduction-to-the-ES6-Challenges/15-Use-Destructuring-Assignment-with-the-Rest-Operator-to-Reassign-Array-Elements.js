86ea8e8a301820d385dc9c7e630e209a2e41b3a5
Joseph Strelnik <jstrelnik@hotmail.com>
2019-08-04 10:04:02 -0600

    adds code and learning summary to complete intro to ES6 challanges exercise 15

---
+++ 6-Introduction-to-the-ES6-Challenges/15-Use-Destructuring-Assignment-with-the-Rest-Operator-to-Reassign-Array-Elements.js
@@ -0,0 +1,13 @@
+//Learned how to use destructuring assignment to collect elements into seperate arrays using the rest element as the last variable in a list of variables
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a, b, ...arr] = list;  // change this
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
